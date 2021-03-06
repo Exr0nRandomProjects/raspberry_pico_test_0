be9ba88fcf3309ebd2b2e108675ca97fcbb432f0
Exr0n <mail@exr0n.com>
2021-03-06 13:52:45 -0800

    UX: progress bar

--- main.c
+++ main.c
@@ -42,6 +42,20 @@ void core1_entry()
     }
 }
 
+void print_progbar(const uint len, const double lo, const double hi, const double pos, const double base_pos)
+{
+    printf("%.2lf |", lo);
+    for (uint i=0; i<len; ++i) {
+        if ((double)i/len <= pos && pos <= (double)(i+1)/len)
+            printf("o");
+        else if ((double)i/len <= base_pos)
+            printf("-");
+        else 
+            printf(" ");
+    }
+    printf("| %.2lf ", hi);
+}
+
 double CLOCK_FREQ = -1;
 const double DRIVE_FACTOR = STEPS_PER_ROTATION*BELT_RATIO;
 double set_rpm(const PIO pio, const uint sm, const double rpm)
@@ -51,7 +65,8 @@ double set_rpm(const PIO pio, const uint sm, const double rpm)
     if (ans < bitbang0_upkeep_instruction_count) return -1; // too fast, not possible
     if (ans > 1e6) return -1;                               // too slow, disallow
     const double reached = (double)30/((unsigned)ans)*CLOCK_FREQ*MICROSTEP/DRIVE_FACTOR;
-    printf("       Settting output to %u ipt (%.2lf tps %.2lf rpm)\r", (unsigned)ans, CLOCK_FREQ/(unsigned)ans/2, reached);
+    //printf("set %u ipt (%.2lf tps %.2lf rpm)\r", (unsigned)ans, CLOCK_FREQ/(unsigned)ans/2, reached);
+    printf("set %.2lftps %.2lfrpm\r", CLOCK_FREQ/(unsigned)ans/2, reached);
     pio_sm_put_blocking(pio, sm, (unsigned)ans-bitbang0_upkeep_instruction_count);
     return reached;
 }
@@ -59,7 +74,7 @@ double set_rpm(const PIO pio, const uint sm, const double rpm)
 double TARGET_RPM = 0.1;
 void ramp(const PIO pio, const uint sm, const double to, const double seconds)
 {
-    printf("Ramping to target %.2lf RPM over %.2lf seconds...                    \n", to, seconds);
+    //printf("Ramping to target %.2lf RPM over %.2lf seconds...\n", to, seconds);
     multicore_fifo_push_blocking(CORECOM_FLASH);
     const double start_ms = to_ms_since_boot(get_absolute_time());
     double cur_ms; unsigned prev=0;
@@ -68,10 +83,13 @@ void ramp(const PIO pio, const uint sm, const double to, const double seconds)
         // y=\frac{\left(\left(a-b\right)\cos\left(\frac{x\pi}{t}\right)+a+b\right)}{2}
         // where a = src, b = dst, t = len
         double inter = ((TARGET_RPM-to)*cos((cur_ms-start_ms)/1e3*M_PI/seconds) + TARGET_RPM + to) / 2;
-        if ((unsigned)inter != prev) set_rpm(pio, sm, inter);    // TODO: delayed due to queue, should calculate how much time will elapse before next and push based on that
+        if ((unsigned)inter != prev) {
+            print_progbar(50, fmin(TARGET_RPM, to), fmax(TARGET_RPM, to), (inter-fmin(to, TARGET_RPM))/fabs(to-TARGET_RPM), (cur_ms - start_ms)/seconds/1e3);
+            set_rpm(pio, sm, inter);    // TODO: delayed due to queue, should calculate how much time will elapse before next and push based on that
+        }
     } while (cur_ms - start_ms < seconds*1e3);
     const double reached = set_rpm(pio, sm, TARGET_RPM = to);
-    printf("Target RPM %lf approximated by %lf.                              \n", TARGET_RPM, reached);
+    printf("Target RPM %lf approximated by %lf.                                                       \n", TARGET_RPM, reached);
     multicore_fifo_push_blocking(CORECOM_SOLID);
 }
 
