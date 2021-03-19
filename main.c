d42d2f498f15f3fe84b8014e1a1b17f48c2844b4
Exr0n <mail@exr0n.com>
2021-03-18 20:48:33 -0700

    kermit

--- main.c
+++ main.c
@@ -11,12 +11,12 @@
 #include "hardware/clocks.h"
 #include "main.pio.h"
 
-//uint MICROSTEP = 1; // 1 or 4 or 8
-//const uint STEPS_PER_ROTATION = 1;
-//const double BELT_RATIO = 1;
-uint MICROSTEP = 8; // 1 or 4 or 8
-const uint STEPS_PER_ROTATION = 200;
-const double BELT_RATIO = 100/7;    // TODO diameter hub / diameter stepper
+uint MICROSTEP = 1; // 1 or 4 or 8
+const uint STEPS_PER_ROTATION = 1;
+const double BELT_RATIO = 1;
+//uint MICROSTEP = 8; // 1 or 4 or 8
+//const uint STEPS_PER_ROTATION = 200;
+//const double BELT_RATIO = 100/7;    // TODO diameter hub / diameter stepper
 
 #define BUILTIN_LED_PIN 25
 const uint OUTPUT_PIN = 15;
@@ -26,22 +26,6 @@ const int CORECOM_FLASH = 1;
 const int CORECOM_SOLID = 2;
 const int CORECOM_ERROR = -1;
 
-void core1_entry()
-{
-    int state = CORECOM_SOLID;
-// main loop
-    while (1) {
-        if (multicore_fifo_rvalid()) state = multicore_fifo_pop_blocking();
-
-        if (state == CORECOM_FLASH) {
-            gpio_put(BUILTIN_LED_PIN, 0);
-            sleep_ms(FLASH_PERIOD);
-        }
-        gpio_put(BUILTIN_LED_PIN, 1);
-        sleep_ms(FLASH_PERIOD);
-    }
-}
-
 void print_progbar(const uint len, const double lo, const double hi, const double pos, const double base_pos)
 {
     printf("%.2lf |", lo);
@@ -50,7 +34,7 @@ void print_progbar(const uint len, const double lo, const double hi, const doubl
             printf("o");
         else if ((double)i/len <= base_pos)
             printf("-");
-        else 
+        else
             printf(" ");
     }
     printf("| %.2lf ", hi);
@@ -64,13 +48,15 @@ double CLOCK_FREQ = -1;
 const double DRIVE_FACTOR = STEPS_PER_ROTATION*BELT_RATIO;
 double calc_delay_time(const double rpm)
 {
+	return 30/rpm * MICROSTEP/DRIVE_FACTOR;
 }
 double set_rpm(const PIO pio, const uint sm, const double rpm)
 {
     if (CLOCK_FREQ < 0) CLOCK_FREQ = pio_clock_freq()/bitbang0_clock_divisor;
-    double ans = 30/rpm *CLOCK_FREQ*MICROSTEP/DRIVE_FACTOR;
+    /* double ans = 30/rpm *CLOCK_FREQ*MICROSTEP/DRIVE_FACTOR; */
+	double ans = calc_delay_time(rpm)*CLOCK_FREQ;
     if (ans < bitbang0_upkeep_instruction_count) return -1; // too fast, not possible
-    if (ans > 1e6) return -1;                               // too slow, disallow
+    if (ans > 1e6) return -1;                               // too slow, disallow TODO: change condition
     const double reached = (double)30/((unsigned)ans)*CLOCK_FREQ*MICROSTEP/DRIVE_FACTOR;
     printf(" %uipt %.2lftps %.2lfrpm)\r", (unsigned)ans, CLOCK_FREQ/(unsigned)ans/2, reached);
     //printf("set %.2lftps %.2lfrpm\r", CLOCK_FREQ/(unsigned)ans/2, reached);
@@ -90,15 +76,40 @@ void ramp(const PIO pio, const uint sm, const double to, const double seconds)
         // y=\frac{\left(\left(a-b\right)\cos\left(\frac{x\pi}{t}\right)+a+b\right)}{2}
         // where a = src, b = dst, t = len
         double inter = ((TARGET_RPM-to)*cos((cur_ms-start_ms)/1e3*M_PI/seconds) + TARGET_RPM + to) / 2;
+        printf("inter %lf, delay %lf\n\n", inter, calc_delay_time(inter));
+        if (calc_delay_time(inter) > 0.2) {                    // set max absolute tick delay to 0.2 seconds
+	        if (to > TARGET_RPM) continue;
+	        else {
+		        printf("!! Target RPM %.2lf too slow! Stopping at %lf RPM !!\n\n", to, inter);
+		        TARGET_RPM = inter;
+		        return;
+	        }
+        }
         if ((unsigned)inter != prev) {
             print_progbar(50, fmin(TARGET_RPM, to), fmax(TARGET_RPM, to), (inter-fmin(to, TARGET_RPM))/fabs(to-TARGET_RPM), (cur_ms - start_ms)/seconds/1e3);
-            set_rpm(pio, sm, inter);    // TODO: delayed due to queue, should calculate how much time will elapse before next and push based on that
-            sleep_ms(inter-elapsed()+cur_ms-1);
+            set_rpm(pio, sm, inter);
+            sleep_ms(calc_delay_time(inter)+cur_ms-elapsed()-1);
         }
     } while (cur_ms - start_ms < seconds*1e3);
     const double reached = set_rpm(pio, sm, TARGET_RPM = to);
     printf("Target RPM %lf approximated by %lf.                                                       \n", TARGET_RPM, reached);
     multicore_fifo_push_blocking(CORECOM_SOLID);
+}
+
+void core1_entry()
+{
+    int state = CORECOM_SOLID;
+// main loop
+    while (1) {
+        if (multicore_fifo_rvalid()) state = multicore_fifo_pop_blocking();
+
+        if (state == CORECOM_FLASH) {
+            gpio_put(BUILTIN_LED_PIN, 0);
+            sleep_ms(FLASH_PERIOD);
+        }
+        gpio_put(BUILTIN_LED_PIN, 1);
+        sleep_ms(FLASH_PERIOD);
+    }
 }
 
 char buf[64];
@@ -122,7 +133,6 @@ int main() {
     const uint offset = pio_add_program(pio, &bitbang0_program);
     bitbang0_init(pio, sm, offset, OUTPUT_PIN, 1);
 
-
 // main loop
     //set_rpm(pio, sm, TARGET_RPM = 240); ramp(pio, sm, 480, 3); // TODO FOR LED TESTING ONLY: remove when using stepper
     while (1) {
@@ -131,4 +141,3 @@ int main() {
         if (!strcmp(buf, "set")) ramp(pio, sm, arg, fmax(3, log10(fabs(arg-TARGET_RPM))*4));
     }
 }
-
